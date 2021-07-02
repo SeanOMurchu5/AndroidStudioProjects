d1bca064473c1dd6f9f3be596a76a063a4fdde18
sean <seanomurchu.5201@gmail.com>
2021-07-02 20:49:05 +0100

    expression fixes  Please enter the commit message for your changes. Lines starting

--- Calculator3/app/src/main/java/com/example/calculator3/MainActivity.java
+++ Calculator3/app/src/main/java/com/example/calculator3/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.calculator3;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import org.mariuszgromada.math.mxparser.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView previousCalculation;
     private EditText display;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +24,17 @@ public class MainActivity extends AppCompatActivity {
 
         previousCalculation = findViewById(R.id.previousCalculationView);
         display = findViewById(R.id.displayEditText);
+
+        display.setShowSoftInputOnFocus(false);
     }
     private void updateText(String strToAdd){
-        display.setText(strToAdd);
+        String oldStr = display.getText().toString();
+
+         int cursorPos = display.getSelectionStart();
+        String leftStr = oldStr.substring(0, cursorPos);
+        String rightStr = oldStr.substring(cursorPos);
+        display.setText(String.format("%s%s%s",leftStr,strToAdd, rightStr));
+        display.setSelection(cursorPos + strToAdd.length());
     }
     public void zeroBTNPush(View view){
         updateText(getResources().getString(R.string.zero));
@@ -53,11 +67,11 @@ public class MainActivity extends AppCompatActivity {
         updateText(getResources().getString(R.string.nine));
     }
     public void clearBTNPush(View view){
-        updateText("");
+     display.setText("");
     }
 
-    public void exponentBTNPush(View view){
-        updateText(getResources().getString(R.string.exponent));
+    public void rightParenthesesBTNPush(View view){
+        updateText(getResources().getString(R.string.rightParentheses));
     }
     public void divideBTNPush(View view){
         updateText(getResources().getString(R.string.divide));
@@ -71,11 +85,34 @@ public class MainActivity extends AppCompatActivity {
     public void addBTNPush(View view){
         updateText(getResources().getString(R.string.add));
     }
-    public void parenthesesBTNPush(View view){
-        updateText(getResources().getString(R.string.parentheses));
+    public void leftParenthesesBTNPush(View view){
+        updateText(getResources().getString(R.string.leftParentheses));
     }
     public void pointBTNPush(View view){
         updateText(getResources().getString(R.string.point));
+    }
+
+    public void backspaceBTNPush(View view){
+        int cursorPos = display.getSelectionStart();
+        int textLen = display.getText().length();
+
+        if(cursorPos != 0 && textLen != 0){
+            SpannableStringBuilder selection = (SpannableStringBuilder)display.getText();
+            selection.replace(cursorPos-1,cursorPos,"");
+            display.setText(selection);
+            display.setSelection(cursorPos-1);
+        }
+    }
+
+    public void equalsBTNPush(View view){
+        String userExp = display.getText().toString();
+        userExp = userExp.replaceAll(getResources().getString(R.string.divide), "/");
+        userExp = userExp.replaceAll(getResources().getString(R.string.multiply), "*");
+
+        Expression exp = new Expression(userExp);
+        String result = String.valueOf(exp.calculate());
+        display.setText(result);
+        display.setSelection(result.length());
     }
 
 
